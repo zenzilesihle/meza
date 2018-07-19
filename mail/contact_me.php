b69edca7ac90915f96430498af9352b3f61b7069
zenzilesihle <sihlezen10@gmail.com>
2018-07-19 21:40:28 +0200

    Changed index

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,7 +11,6 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 
-echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 
